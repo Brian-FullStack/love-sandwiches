ae7b5686c632d8d74dc1aa3b1f22b7d31759981e
Brian-FullStack <brian.fullstack@gmail.com>
2021-12-13 12:53:41 -0500

    Add get stock values function to make a dictonary of stock to make

--- run.py
+++ run.py
@@ -134,6 +134,19 @@ def calculate_stock_data(data):
         new_stock_data.append(round(stock_num))
 
     return new_stock_data
+
+def get_stock_values(data):
+    """
+    Tell the user how much stock to prepare for the next market.
+    """
+    print("Make the following number of sandwiches for next market:\n")
+    headings = SHEET.worksheet("stock").get_all_values()
+    headings_row = headings[0]    
+    stock_dict = dict(zip(headings_row, data))
+    
+    print(stock_dict)
+    
+
 def main():
     """
     Run all program functions
@@ -146,7 +159,7 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    get_stock_values(stock_data)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
-
